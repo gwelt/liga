1d90162c0d057f8042b39b35cbb5184c3ef9e529
Sven <sven@gwelt.net>
2017-09-14 21:41:50 +0200

    paths changed

--- ligaserver.js
+++ ligaserver.js
@@ -8,36 +8,59 @@ config.ligalist.forEach((liga)=>{
 const express = require('express');
 const PORT = process.env.PORT || config.port;
 const server = express()
-	.get('/:id?/:call?', (req,res)=>{
+	.get('/:id?/:call?/:subcall?', (req,res)=>{
 		res.setHeader('Access-Control-Allow-Origin','*');
 		if (req.params.id) {
 			var liga=get_liga_by_id(req.params.id);
 			if (liga) {
 				switch (req.params.call) {
+
 					case undefined:
-						res.send(liga.print_liga_table()+'\n'+liga.print_matches()+'\n'+liga.print_matches(liga.active_matchday+1)+'\n'+liga.last_update);
-						break;
-					case 'tabelle':
-						res.send(liga.print_liga_table());
-						break;
-					case 'spiele':
-						res.send(liga.print_matches()+'\n'+liga.print_matches(liga.active_matchday+1));
-						break;
-					case 'json':
-						res.json(liga);
-						break;
-					case 'html':
 						res.send('<pre>'+liga.print_liga_table()+'\n'+liga.print_matches()+'\n'+liga.print_matches(liga.active_matchday+1)+'\n'+liga.last_update+'</pre>');
 						break;
-					case 'update':
-						liga.load(()=>{res.send('UPDATED. '+liga.last_update)});
+					
+					case 'print':
+						switch (req.params.subcall) {
+							case undefined:
+								res.send(liga.print_liga_table()+'\n'+liga.print_matches()+'\n'+liga.print_matches(liga.active_matchday+1)+'\n'+liga.last_update);
+								break;
+							case 'tabelle':
+								res.send(liga.print_liga_table());
+								break;
+							case 'spiele':
+								res.send(liga.print_matches()+'\n'+liga.print_matches(liga.active_matchday+1));
+								break;
+							default:
+							res.sendStatus(404);
+						}
 						break;
+
+					case 'json':
+						switch (req.params.subcall) {
+							case undefined:
+								res.json(liga);
+								break;
+							case 'tabelle':
+								res.json(liga.teams);
+								break;
+							case 'spiele':
+								res.json(liga.matchdays);
+								break;
+							default:
+							res.sendStatus(404);
+						}
+						break;
+					
 					case 'check':
 						liga.check((needs_update)=>{
 							if (needs_update>0) {liga.load(()=>{res.send('CHECKED (='+needs_update+') AND UPDATED. '+liga.last_update)})}
 							else {res.send('CHECKED. NO UPDATE NEEDED. '+liga.last_update)}
 						});
 						break;
+					case 'update':
+						liga.load(()=>{res.send('UPDATED. '+liga.last_update)});
+						break;
+					
 					default:
 						res.sendStatus(404);
 				}
@@ -47,7 +70,7 @@ const server = express()
 		else {
 			var out='';
 			ligalist.forEach((liga)=>{
-				out+='<a href=/'+liga.id+'/html>'+liga.leaguename+'</a> | <a href=/'+liga.id+'/json>JSON</a> | <a href=/'+liga.id+'/check>check for update</a> ('+liga.last_update+')<br>';
+				out+='<h1><a href=/'+liga.id+'>'+liga.leaguename+'</a></h1><li><a href=/'+liga.id+'/print>/'+liga.id+'/print</a> Tabelle und Spiele formatiert für Thermo-Drucker</li><li><a href=/'+liga.id+'/print/tabelle>/'+liga.id+'/print/tabelle</a> Tabelle formatiert für Thermo-Drucker</li><li><a href=/'+liga.id+'/print/spiele>/'+liga.id+'/print/spiele</a> Spiele formatiert für Thermo-Drucker</li><li><a href=/'+liga.id+'/json>/'+liga.id+'/json</a> Tabelle und Spiele im JSON-Format</li><li><a href=/'+liga.id+'/json/tabelle>/'+liga.id+'/json/tabelle</a> Tabelle im JSON-Format</li><li><a href=/'+liga.id+'/json/spiele>/'+liga.id+'/json/spiele</a> Spiele im JSON-Format</li><li><a href=/'+liga.id+'/check>/'+liga.id+'/check</a> check for update</li><li><a href=/'+liga.id+'/update>/'+liga.id+'/update</a> force update</li><br>Last change: '+liga.last_update+'<br>';
 			});
 			res.send(out)
 		}
